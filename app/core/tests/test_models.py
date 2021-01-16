9771ee3dedb024e84e09f8d617275ecfec2cc685
domfarnham <dominic.farnham@hotmail.co.uk>
2021-01-16 05:59:01 +0000

    Updated models.py & test_models.py.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,12 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for a new user is normalized
+        """
+        email = 'test@TESTING.com'
+        user = get_user_model().objects.create_user(email, 'testing123')
+
+        self.assertEqual(user.email, email.lower())
